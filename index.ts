bfc1875f12bde21b9cdbe2ec5fe56a2a56fb5bf0
chirag-ghosh <cghosh828049@gmail.com>
2022-08-14 09:41:58 +0530

    Create mvc structure;

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+import testRoute from './routes/health'
+
 dotenv.config()
 const app = express()
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.json({
-        'PBL': 'Up and Running!'
-    })
-})
+app.use('/', testRoute)
 
 app.listen(process.env.PORT, () => {
     console.log(`Mike Ross is up and running at PORT: ${process.env.PORT}`)

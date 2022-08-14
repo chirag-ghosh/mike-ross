97ec01d524294012169307d80a5c5e223c732bda
chirag-ghosh <cghosh828049@gmail.com>
2022-08-14 10:27:35 +0530

    Add test db entry;

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
-import testRoute from './routes/health'
+import dbConnect from './utils/database'
+
+import healthRoute from './routes/health'
+import testRoute from './routes/test'
 
 dotenv.config()
 const app = express()
 
-app.use('/', testRoute)
+dbConnect()
+    .then(() => console.log('Database connected.'))
+    .catch((err) => console.log('Error in database connection: ', err))
+
+app.use('/', healthRoute)
+app.use('/test', testRoute)
 
 app.listen(process.env.PORT, () => {
     console.log(`Mike Ross is up and running at PORT: ${process.env.PORT}`)
